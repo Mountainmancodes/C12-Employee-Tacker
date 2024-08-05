445ab1b61c0fb332accabce773b5f13e6f2f81a8
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-08-04 18:45:02 -0600

    update comments for seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- Insert initial data into the 'department' table
 INSERT INTO department (name) VALUES 
 ('Information Technology'),
 ('Sales'),
@@ -5,7 +6,7 @@ INSERT INTO department (name) VALUES
 ('Finance'),
 ('Human Resources'),
 ('Legal');
-
+-- Insert initial data into the 'role' table
 INSERT INTO role (title, salary, department_id) VALUES
 ('Software Engineer', 90000, 1),
 ('Sales Manager', 80000, 2),
@@ -13,7 +14,7 @@ INSERT INTO role (title, salary, department_id) VALUES
 ('Financial Analyst', 65000, 4),
 ('Benefits Administrator', 75000, 5),
 ('Personal Injury Lawyer', 185000, 6);
-
+-- Insert initial data into the 'employee' table
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('Osef', 'Barrens', 1, NULL),
 ('Julia', 'Warts', 2, NULL),
@@ -22,7 +23,7 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('Mikal', 'Cyprus', 6, 3),
 ('Joshua', 'White', 4, NULL),
 ('Sarah', 'Strickland', 3, 2);
-
+-- Select all data from department, role, and employee to verify inserts
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
